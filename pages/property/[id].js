1948f865409af1b440594a2998136dbff48b60f0
Richard Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-04-21 14:50:19 +0200

    Update [id].js

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -15,7 +15,7 @@ const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title
       <Flex paddingTop='2' alignItems='center'>
         <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
         <Text fontWeight='bold' fontSize='lg'>
-          CDF {price} {rentFrequency && `/${rentFrequency}`}
+          AED {price} {rentFrequency && `/${rentFrequency}`}
         </Text>
         <Spacer />
         <Avatar size='sm' src={agency?.logo?.url}></Avatar>
